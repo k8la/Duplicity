9d298fd46e75b6be0bf81386c6d070534c8d71dd
k8la <kaitlynla82@gmail.com>
2015-03-18 19:10:55 -0700

    connected storyboard to postviewcontroller

--- Dupe/ListViewController.swift
+++ Dupe/ListViewController.swift
@@ -1,15 +1,16 @@
+////
+////  ListViewController.swift
+////  Duplicity
+////
+////  Created by Kaitlyn on 3/18/15.
+////  Copyright (c) 2015 K8La. All rights reserved.
+////
 //
-//  ListViewController.swift
-//  Duplicity
+//import UIKit
 //
-//  Created by Kaitlyn on 3/18/15.
-//  Copyright (c) 2015 K8La. All rights reserved.
+//class ListViewController: UITableViewController {
+//    
+//    
 //
-
-import UIKit
-
-class ListViewController: UITableViewController {
-    
-    
-
-}
+//}
+//
